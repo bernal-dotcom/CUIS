b2301722882f3c85a999c28da3e324b814df7597
bernal-dotcom <cesar.bernal.ti@gmail.com>
2025-06-20 15:59:35 -0600

    UI prototype

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SectorViewSet, WaterSystemViewSet, ElectrocalSubstationViewSet, AlertViewSet
+from .views import SectorViewSet, WaterSystemViewSet, ElectricalSubstationViewSet, AlertViewSet, main_panel, sectors_view, water_system_view, substation_view
 
 router = DefaultRouter()
 router.register(r'sector', SectorViewSet)
 router.register(r'water-system', WaterSystemViewSet)
-router.register(r'substation', ElectrocalSubstationViewSet)
+router.register(r'substation', ElectricalSubstationViewSet)
 router.register(r'alert', AlertViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', main_panel, name='panel'),
+    path('sectors/', sectors_view, name="sectors"),
+    path('waters-systems/', water_system_view, name="water"),
+    path('substations/', substation_view, name="substation"),
+    path('api/', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ class WaterSystemViewSet(viewsets.ModelViewSet):
     serializer_class = WaterSystemSerializer
 
 
-class ElectrocalSubstationViewSet(viewsets.ModelViewSet):
+class ElectricalSubstationViewSet(viewsets.ModelViewSet):
     queryset = ElectricalSubstation.objects.all()
     serializer_class = ElectricalSubstationSerializer
 
@@ -21,3 +21,19 @@ class ElectrocalSubstationViewSet(viewsets.ModelViewSet):
 class AlertViewSet(viewsets.ModelViewSet):
     queryset = Alert.objects.all()
     serializer_class = AlertSerializer
+
+
+def main_panel(request):
+    return render(request, 'main_panel.html')
+
+def sectors_view(request):
+    sectors = Sector.objects.all()
+    return render(request, 'sectors.html', {'sectors': sectors})
+
+def water_system_view(request):
+    data = WaterSystem.objects.all().order_by('-timestamp')[:10]
+    return render(request, 'water_systems.html', {'data': data})
+
+def substation_view(request):
+    substations = ElectricalSubstation.objects.all().order_by('-timestamp')[:10]
+    return render(request, 'substations.html', {'substations': substations })
